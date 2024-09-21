5a71344219e5132bd1b224cc396733acf5dc1942
mikoto2000 <mikoto2000@gmail.com>
2024-09-21 10:47:50 +0000

    Update CommissionMaster's db constraint.

--- db/migrate/20240921104353_create_commission_masters.rb
+++ db/migrate/20240921104353_create_commission_masters.rb
@@ -1,11 +1,17 @@
 class CreateCommissionMasters < ActiveRecord::Migration[7.2]
   def change
     create_table :commission_masters do |t|
-      t.date :from_date
-      t.date :to_date
-      t.decimal :commission_fee
+      t.date :from_date, null: false
+      t.date :to_date, null: false
+      t.decimal :commission_fee, null: false
 
       t.timestamps
     end
+
+    execute <<-SQL
+        ALTER TABLE commission_masters
+        ADD CONSTRAINT exclude_overlapping_commission_masters
+        EXCLUDE USING gist (daterange(from_date, to_date, '[]') WITH &&);
+      SQL
   end
 end
