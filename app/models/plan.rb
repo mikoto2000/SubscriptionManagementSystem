c1981ba5a4e20be3b0049cd0f10b2fcabb5be675
mikoto2000 <mikoto2000@gmail.com>
2024-09-22 09:35:00 +0000

    Added Plan's validation.

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,5 +2,8 @@ class Plan < ApplicationRecord
   def self.ransackable_attributes(_auth_object = nil)
     %w[publisher_id name cost id created_at updated_at]
   end
+
+  validates :publisher, uniqueness: { scope: :name }
+
   belongs_to :publisher
 end
