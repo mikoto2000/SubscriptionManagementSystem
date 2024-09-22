39d8ba6689b12b8c80327d0824c806ef25b4fa26
mikoto2000 <mikoto2000@gmail.com>
2024-09-22 05:39:16 +0000

    Added Subscriber's constraint.

--- db/migrate/20240922053642_create_subscribers.rb
+++ db/migrate/20240922053642_create_subscribers.rb
@@ -1,10 +1,12 @@
 class CreateSubscribers < ActiveRecord::Migration[7.2]
   def change
     create_table :subscribers do |t|
-      t.string :name
-      t.string :email_address
+      t.string :name, null: false
+      t.string :email_address, null: false
 
       t.timestamps
     end
+
+    add_index :subscribers, :email_address, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_22_033048) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_22_053642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,5 +30,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_22_033048) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_publishers_on_email_address", unique: true
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string "name"
+    t.string "email_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_subscribers_on_email_address", unique: true
   end
 end
