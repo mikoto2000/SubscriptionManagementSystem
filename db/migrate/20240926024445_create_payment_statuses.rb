36d1bef30308fbd09862c0434d499e51839c2c05
mikoto2000 <mikoto2000@gmail.com>
2024-09-26 07:31:49 +0000

    Added PaymentStatus's constraint.

--- db/migrate/20240926024445_create_payment_statuses.rb
+++ db/migrate/20240926024445_create_payment_statuses.rb
@@ -1,7 +1,7 @@
 class CreatePaymentStatuses < ActiveRecord::Migration[7.2]
   def change
     create_table :payment_statuses do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
