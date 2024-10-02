db59b59938497445f109400507a11d8e27bd96f1
mikoto2000 <mikoto2000@gmail.com>
2024-10-02 06:52:35 +0000

    ホームに、その月に購読していた(解除済み含む)プランを表示するように修正。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,18 @@ class HomeController < ApplicationController
     @publish_plans = Plan.where(publisher_id: current_account.publisher_id)
     @subscribe_subscriptions = Subscription
       .where(subscriber_id: current_account.subscriber_id)
-      .where("end_date >= ?", Time.zone.today)
+      .where("end_date >= ?", Time.zone.today.beginning_of_month)
       .or(Subscription.where(end_date: nil))
       .eager_load(:plan)
   end
 
   def create_plan
     @publish_plans = Plan.where(publisher_id: current_account.publisher_id)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
